b81c0238bdb23012d45516e2cfbeadad2201c2d4
tark-hidden <tark.hidden@mail.ru>
2014-04-24 12:17:54 -0700

    Update pyramide_classy.py

--- pyramide_classy.py
+++ pyramide_classy.py
@@ -29,16 +29,13 @@ class ClassyView(object):
 
     @classmethod
     def register(cls, config, route_base=None):
-        cn = cls.__name__
-        cls_name = (cn[:-4] if len(cn) > 4 and cn.endswith('View')
-                    else cn).lower()
-        if route_base:
-            cls.route_base = route_base
-        if cls.route_base == '/' or not cls.route_base:
-            cls.route_base = '' if cls_name == 'index' \
-                else '/' + cls_name.strip('/')
+        n = cls.__name__
+        prefix = (n[:-4] if len(n) > 4 and n.endswith('View') else n).lower()
         if route_base and route_base != '/':
-            cls_name = '%s.%s' % (cls_name, route_base.strip('/'))
+            cls.route_base = '/%s' % route_base.lstrip('/')
+            prefix = '%s.%s' % (prefix, route_base.strip('/'))
+        if cls.route_base == '/':
+            cls.route_base = '' if prefix == 'index' else '/%s' % prefix
 
         for name, _ in get_members(ClassyView, cls):
             function = cls.make_proxy_method(name)
@@ -50,7 +47,7 @@ class ClassyView(object):
                     route_name = options.pop('route_name', None)
 
                     if not route_name:
-                        route_name = '%s.%s' % (cls_name, name)
+                        route_name = '%s.%s' % (prefix, name)
                     if len(rules) > 1:
                         route_name = '%s_%d' % (route_name, idx)
 
@@ -58,7 +55,7 @@ class ClassyView(object):
                     config.add_view(function, route_name=route_name, **options)
             else:  # no decorators
                 url = cls.build_url(name, name)
-                route_name = '%s.%s' % (cls_name, name)
+                route_name = '%s.%s' % (prefix, name)
                 config.add_route(route_name, url)
                 config.add_view(function, route_name=route_name)
 
