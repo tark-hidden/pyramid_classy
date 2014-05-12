bf135755165469782c4149a3228f86e6cf32113f
tark-hidden <tark.hidden@mail.ru>
2014-05-12 11:11:50 -0700

    Update pyramide_classy.py

--- pyramide_classy.py
+++ pyramide_classy.py
@@ -25,13 +25,15 @@ def get_members(base, current):
 
 class ClassyView(object):
     route_base = '/'
+    debug = False
+    __view_defaults__ = {}
 
     def __init__(self, request, context=None):
         self.request = request
         self.context = context
 
     @classmethod
-    def register(cls, config, route_base=None):
+    def register(cls, config, route_base=None, debug=False):
         n = cls.__name__
         prefix = (n[:-4] if len(n) > 4 and n.endswith('View') else n).lower()
         if route_base and route_base != '/':
@@ -39,31 +41,42 @@ class ClassyView(object):
             prefix = '%s.%s' % (prefix, route_base.strip('/'))
         if cls.route_base == '/':
             cls.route_base = '' if prefix == 'index' else '/%s' % prefix
+        if debug:
+            cls.debug = debug
 
+        routes = []
         for name, _ in get_members(ClassyView, cls):
+            view_defaults = cls.__view_defaults__.copy()
             if hasattr(getattr(cls, name), 'rules'):
                 rules = getattr(cls, name).rules
                 for idx, rule in enumerate(rules):
                     url, options = rule
                     url = cls.build_url(name, url)
                     route_name = options.pop('route_name', None)
+                    view_defaults.update(options or {})
+                    options = view_defaults
 
                     if not route_name:
                         route_name = '%s.%s' % (prefix, name)
                     if len(rules) > 1:
                         route_name = '%s_%d' % (route_name, idx)
-
-                    config.add_route(route_name, url)
-                    config.add_view(cls, attr=name,
-                                    route_name=route_name, **options)
+                    routes.append((url, name, route_name, options))
             else:  # no decorators
                 url = cls.build_url(name, name)
                 route_name = '%s.%s' % (prefix, name)
-                config.add_route(route_name, url)
-                config.add_view(cls, attr=name, route_name=route_name)
+                options = view_defaults
+                routes.append((url, name, route_name, options))
+
+        for rule in routes:
+            url, name, route_name, options = rule
+            config.add_route(route_name, url)
+            config.add_view(cls, attr=name, route_name=route_name, **options)
+            if cls.debug:
+                print "%s%s '%s'" % (cls.route_base, url, route_name)
+
 
     @classmethod
     def build_url(cls, name, url=''):
-        if name == 'index':
+        if name == 'index' and name == url:
             url = ''
         return '%s/%s' % (cls.route_base, url.lstrip('/'))
